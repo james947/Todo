51b89c80b210b0328bae721fc48980ae2cdab435
james947 <jmuriuki947@gmail.com>
2020-02-23 13:14:58 +0300

    FInishes quasar app

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,148 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todos</div>
+        <div class="text-subtitle1">{{todaysDate}}</div>
+      </div>
+      <q-img class="header-image absolute-top" src="statics/coffee.jpg"></q-img>
+    </q-header>
+
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+            to="/"
+            exact
+            clickable
+            v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+            <q-item
+            to="/help"
+            exact
+            clickable
+             v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics/coffee.jpg" style="height: 192px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img class="avtr" src="https://i.imgur.com/UsT2OPA.jpg?1">
+            </q-avatar>
+            <div class="text-weight-bold">James Agushoma</div>
+            <div>@jim_muriuki</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from 'components/EssentialLink'
+import { date } from 'quasar'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Docs',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Github',
+          caption: 'github.com/quasarframework',
+          icon: 'code',
+          link: 'https://github.com/quasarframework'
+        },
+        {
+          title: 'Discord Chat Channel',
+          caption: 'chat.quasar.dev',
+          icon: 'chat',
+          link: 'https://chat.quasar.dev'
+        },
+        {
+          title: 'Forum',
+          caption: 'forum.quasar.dev',
+          icon: 'record_voice_over',
+          link: 'https://forum.quasar.dev'
+        },
+        {
+          title: 'Twitter',
+          caption: '@quasarframework',
+          icon: 'rss_feed',
+          link: 'https://twitter.quasar.dev'
+        },
+        {
+          title: 'Facebook',
+          caption: '@QuasarFramework',
+          icon: 'public',
+          link: 'https://facebook.quasar.dev'
+        }
+      ]
+    }
+  },
+  computed: {
+    todaysDate () {
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .header-image{
+    height: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter: grayscale(100%)
+  }
+  .avtr{
+    object-fit: fill;
+  }
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ut eum corrupti cumque dolorum animi dignissimos est deserunt quis explicabo doloribus pariatur numquam, autem libero nesciunt praesentium nihil, at facilis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit praesentium quaerat esse unde repellat voluptatem, non deserunt? Minima temporibus vel, modi dolores eaque atque veniam, non voluptate culpa quod sed!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
